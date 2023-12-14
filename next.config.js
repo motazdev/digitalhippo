28a66309a24964196f166a4571a9d05191b0f00d
motazdev <motazessamdev@gmail.com>
2023-12-14 19:07:09 +0200

    final

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
-        protocol: "http",
-      },
-    ],
+    domains: ["localhost", "digitalhippo-production-c5fa.up.railway.app"],
+    // remotePatterns: [
+    //   {
+    //     hostname: "localhost",
+    //     pathname: "**",
+    //     port: "3000",
+    //     protocol: "http",
+    //   },
+    // ],
   },
 };
 
