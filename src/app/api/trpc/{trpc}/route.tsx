cce1d9d1d2dbac1d58e184ee49d6c7d904b3f2c2
motaz essam <53120830+motazdev@users.noreply.github.com>
2024-08-09 23:08:50 +0300

    Update route.tsx

--- src/app/api/trpc/{trpc}/route.tsx
+++ src/app/api/trpc/{trpc}/route.tsx
@@ -1,13 +1,12 @@
 import { appRouter } from "@/trpc";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-
 const handler = async (req: Request) => {
   try {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
     const response = await fetchRequestHandler({
       endpoint: "/api/trpc",
       req,
       router: appRouter,
+      // @ts-expect-error context already passed from express middleware
       createContext: () => ({}),
     });
     console.log(`Response: ${response}`);
