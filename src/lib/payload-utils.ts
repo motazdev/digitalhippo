c983db004ad94ef0dc4503024fb852c8807fda14
motaz essam <53120830+motazdev@users.noreply.github.com>
2024-08-09 19:20:58 +0300

    Update payload-utils.ts

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,16 +1,16 @@
 import { User } from "../payload-types";
 import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
+import payload from "payload";
 
 export const getServerSideUser = async (
   cookies: NextRequest["cookies"] | ReadonlyRequestCookies
 ) => {
   const token = cookies.get("payload-token")?.value;
-  console.log("token: ", token);
-  console.log(
-    "process.env.NEXT_PUBLIC_SERVER_URL: ",
-    process.env.NEXT_PUBLIC_SERVER_URL
-  );
+  console.log("cookies: ", cookies);
+  console.log("cookies ALL: ", cookies.getAll());
+  console.log("payload.authenticate: ", payload.authenticate);
   const meRes = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
     {
