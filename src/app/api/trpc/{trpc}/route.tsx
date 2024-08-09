00a03fd040fe3d9246b2d2f5efdeb83fbab1419b
motaz essam <53120830+motazdev@users.noreply.github.com>
2024-08-09 23:00:41 +0300

    Update route.tsx

--- src/app/api/trpc/{trpc}/route.tsx
+++ src/app/api/trpc/{trpc}/route.tsx
@@ -2,7 +2,7 @@ import { appRouter } from "@/trpc";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 const handler = (req: Request) => {
   fetchRequestHandler({
-    endpoint: "https://digitalhippo-wheat.vercel.app/api/trpc",
+    endpoint: "/api/trpc",
     req,
     router: appRouter,
     // @ts-expect-error context already passed from express middleware
